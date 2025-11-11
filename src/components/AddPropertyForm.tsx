16c26442c6bdb563de6df823942cae978a500407
ToobaSahar <toobasahar02@gmail.com>
2025-11-11 21:55:49 +0500

    11/11 9:55

--- src/components/AddPropertyForm.tsx
+++ src/components/AddPropertyForm.tsx
@@ -4,7 +4,7 @@ import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
-
+//hello
 const propertySchema = z.object({
   // Basic Details
   propertyName: z.string().min(1, 'Property name is required'),
